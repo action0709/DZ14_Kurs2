aba29e0d55ca61c267b7574228cbc6ee96c3b580
action0709 <action0709@mail.ru>
2023-07-08 22:50:57 +0400

    1

---
+++ src/main/java/com/pro/sky/dz14_kurs2/Dz14Kurs2Application.java
@@ -0,0 +1,13 @@
+package com.pro.sky.dz14_kurs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz14Kurs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz14Kurs2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pro/sky/dz14_kurs2/Dz14Kurs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.dz14_kurs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz14Kurs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
